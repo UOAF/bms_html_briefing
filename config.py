c8fd0d8dc2a751917d46a21432f98653a04ef348
wsywsy <dormouse.de@gmail.com>
2025-09-23 22:47:51 +0200

    .gitignore fixed

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Put the location of your BMS install here, e.g. "C:\\Games\\Falcon BMS 4.37"
-bms_location = "/home/tolmak/games/bms_4.37_c/Falcon BMS 4.37/"
-callsign = "wsy"
+bms_location = "C:\\Games\\Falcon BMS 4.37"
+callsign = "pilot"
 
 page_contents = [
     ["airbase", "package", "flightplan"],
